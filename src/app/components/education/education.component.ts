036c15d21ee213d9047c9e214a4f65b7dbacc87d
Jin Wei <seetohjinwei@gmail.com>
2022-05-07 19:31:48 +0800

    css for education

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { educations } from 'src/app/data/Educations';
-import Education from 'src/app/interfaces/IEducation';
 
 @Component({
   selector: 'app-education',
@@ -10,17 +8,5 @@ import Education from 'src/app/interfaces/IEducation';
 export class EducationComponent implements OnInit {
   constructor() {}
 
-  educations: Education[] = educations;
-
   ngOnInit(): void {}
-
-  formatPeriod(edu: Education): string {
-    const formatDate = (month: string | undefined, year: string): string => {
-      return `${month || ''} ${year}`;
-    };
-
-    const start: string = formatDate(edu.startMonth, edu.startYear);
-    const end: string = formatDate(edu.endMonth, edu.endYear);
-    return `${start} - ${end}`;
-  }
 }
--- src/app/interfaces/IEducation.ts
+++
@@ -1,7 +0,0 @@
-export default interface Education {
-  name: string;
-  startYear: string;
-  startMonth?: string;
-  endYear: string;
-  endMonth?: string;
-}
